d2c4db7d8b94a6503f9174c1b80dbb5309d7eacd
Riken Patel <rikenpatel@Rikens-MacBook-Pro.local>
2017-05-06 02:50:21 -0400

    Parsing JSON data

--- src/components/appBody.js
+++ src/components/appBody.js
@@ -1,39 +1,36 @@
 import React, { Component } from 'react';
 import { Text } from 'react-native';
 import { Content, Card, CardItem, Body } from 'native-base';
+import AppBodyData from './appBodyData';
 
 export default class AppBody extends Component {
+
+  constructor() {
+    super()
+    this.state = {
+      data: []
+    }
+  }
+
+  getData() {
+  return fetch('https://www.thewallscript.com/blogfeed/javascript/10')
+    .then((response) => response.json())
+    .then((responseJson) => {
+      this.setState({data: responseJson.feed.entry });
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+
+  }
+
+  componentDidMount() {
+      this.getData();
+  }
+
   render() {
     return (
-      <Content>
-        <Card>
-          <CardItem>
-              <Body>
-                <Text>
-                    My Project Text Card 1
-                </Text>
-              </Body>
-            </CardItem>
-          </Card>
-          <Card>
-            <CardItem>
-                <Body>
-                  <Text>
-                      My Project Text Card 2
-                  </Text>
-                </Body>
-              </CardItem>
-            </Card>
-            <Card>
-              <CardItem>
-                  <Body>
-                    <Text>
-                        My Project Text Card 3
-                    </Text>
-                  </Body>
-                </CardItem>
-              </Card>
-        </Content>
+      <AppBodyData data = {this.state.data} />
     );
   }
 }
---
+++ src/components/appBodyData.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import { Text } from 'react-native';
+import HTMLView from 'react-native-htmlview';
+import { Content, Card, CardItem, Body, Left, Thumbnail } from 'native-base';
+
+export default class AppBodyData extends Component {
+  render(){
+
+    let articles = this.props.data.map(function(articleData, index) {
+        return (
+          <Card>
+                <CardItem>
+                <Left>
+                    <Thumbnail source={require('../img/logo.png')}/>
+                    <Body>
+                        <Text>{articleData.title.$t}</Text>
+                    </Body>
+                </Left>
+                </CardItem>
+                <CardItem content>
+                    <HTMLView value = {articleData.content.$t} />
+                </CardItem>
+            </Card>
+        )
+    });
+
+    return (
+      <Content>
+          {articles}
+        </Content>
+    );
+  }
+}
+
+module.export = AppBodyData;
